94dd7ec4f9750ab3984f180cd76880d006198141
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-09-07 00:49:24 +0200

    chore: add github deploy key

--- scripts/deploy_to_git.sh
+++ scripts/deploy_to_git.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# source https://gist.github.com/qoomon/41697079eef82a56f8de51357c7ee9d9
-
 if [ $# -ne 3 ]; then
     echo "Usage: ... <SOURCE_FOLDER> <TARGET_REPO> <TARGET_BRANCH>"
     exit 1;
@@ -14,7 +12,7 @@ TARGET_REPO="$2"
 TARGET_BRANCH="$3"
 COMMIT_MESSAGE="$(git log -1 --format='Commit: %h - %s%nAuthor: %an <%ae>')\n$(git config --get remote.origin.url)"
 
-echo "Deploy '$SOURCE_FOLDER' to '${TARGET_REPO}' '$TARGET_BRANCH'"
+echo "Deploy '${SOURCE_FOLDER}' to '${TARGET_REPO}' '${TARGET_BRANCH}'"
 
 echo ''
 echo "--- Change directory to ${SOURCE_FOLDER}"
@@ -23,28 +21,13 @@ cd "${SOURCE_FOLDER}";
 echo ''
 echo "--- Init Repository"
 rm -rf '.git'
-git init
-git config user.name "$(whoami)"		
-git config user.email '<>'
+git clone --single-branch --branch "${TARGET_BRANCH}" --depth 1 "${TARGET_REPO}" '.git.shadow'
+cp -R '.git.shadow/.git' './' && rm -rf '.git.shadow'
 
 echo ''
-echo '--- Stash Files'
-git commit --allow-empty -m '.' --quiet
-git add .
-git stash --quiet
-
-echo ''
-echo '--- Pull Target Repository'
-git branch --move "${TARGET_BRANCH}"
-git remote add 'origin' "${TARGET_REPO}"
-git pull --rebase --no-tags --depth 1 'origin' "${TARGET_BRANCH}"
-git branch --set-upstream-to="origin/${TARGET_BRANCH}" --quiet
-
-echo ''
-echo '--- Update to Stash & Commit Changes'
-git rm -r . --ignore-unmatch --quiet
-git stash pop --quiet
-if git commit -am "$COMMIT_MESSAGE" --quiet; then
+echo '--- Add & Commit Changes'
+git add *
+if git commit -am "${COMMIT_MESSAGE}" --quiet; then
     git --no-pager log -n 1 --name-status --stat --oneline
 
     echo ''

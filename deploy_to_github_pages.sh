fc3305306d1c44e596146780d11e8cf7505084e6
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-12-21 00:12:25 +0100

    build: switch to github actions

--- deploy_to_github_pages.sh
+++ deploy_to_github_pages.sh
@@ -1,42 +1,58 @@
 #!/bin/sh
 set -e
 
-if [ $# -ne 4 ]; then
-    echo "Usage: ... <SOURCE_FOLDER> <TARGET_REPO> <TARGET_BRANCH> <COMMIT_USER_NAME> "
-    exit 1;
+if [ $# -lt 1 ]
+then
+    echo "Usage: ... <SOURCE_FOLDER> <TARGET_BRANCH> <TARGET_REPO> <TARGET_BRANCH_REWRITE>"
+    exit 1
 fi
 
 SOURCE_FOLDER="$1"
-TARGET_REPO="$2"
-TARGET_BRANCH="$3"
-COMMIT_USER_NAME="$4"
+TARGET_BRANCH="${2:-'gh-pages'}"
+TARGET_REPO="${3:-"$(git config --get remote.origin.url)"}"
+TARGET_BRANCH_REWRITE="${4:-'false'}"
 COMMIT_MESSAGE="$(git log -1 --format='%s')"
-COMMIT_DETAILS="repository $(git config --get remote.origin.url)"$'\n'"$(git log -1 --format='commit %h%nAuthor: %an <%ae>%nDate:  %d')"
+COMMIT_DETAILS="repository $(git config --get remote.origin.url)"$'\n'"$(git log -1 --format='commit %h%nAuthor: %an <%ae>%nDate:  %ad')"
 
 echo "Deploy '${SOURCE_FOLDER}' to '${TARGET_REPO}' Branch: '${TARGET_BRANCH}'"
 
+
 echo ''
-echo "--- Clone and Prepare Repository"
+echo "--- Checkout Target Branch"
 target_repo_dir="$(mktemp -d)"
+git clone --single-branch --branch "${TARGET_BRANCH}" --depth 1 "${TARGET_REPO}" "${target_repo_dir}"
+
+echo ''
+echo '--- Commit Changes'
 (
-  git clone --single-branch --branch "${TARGET_BRANCH}" --depth 1 "${TARGET_REPO}" "${target_repo_dir}"
-  cd "${target_repo_dir}"
-  #git remote set-url --push 'origin' "${TARGET_REPO}"
-  git config user.name "${COMMIT_USER_NAME}"
-  git config user.email "<>"
-  git rm -rf . --quiet
+  cd "${SOURCE_FOLDER}"
+  export GIT_DIR="${target_repo_dir}/.git"
+  (
+    export GIT_WORK_TREE="${target_repo_dir}"
+    git rm -rf . --quiet
+  )
+  git add . 
+  if git commit --quiet -am "${COMMIT_MESSAGE}" -m "${COMMIT_DETAILS}"
+  then
+      git show --name-status --format=format:
+      
+      echo ''
+      echo '--- Push Changes'
+      if [ "$TARGET_BRANCH_REWRITE" != 'rewrite' ]
+      then
+        git push 'origin' "${TARGET_BRANCH}"
+      else
+        echo 'Rewrite Branch'
+        git checkout --orphan 'tmp'       # Creates new temporary branch
+        git commit --quiet -am "${COMMIT_MESSAGE}" -m "${COMMIT_DETAILS}"
+        git branch -D "${TARGET_BRANCH}"  # Deletes the TARGET_BRANCH branch
+        git branch -m "${TARGET_BRANCH}"  # Rename the temporary branch to TARGET_BRANCH
+        git push --force 'origin' "${TARGET_BRANCH}" # Force push TARGET_BRANCH branch
+      fi
+  fi
 )
 
 echo ''
-echo '--- Apply Changes'
-cp -Rp "${SOURCE_FOLDER}/"* "${target_repo_dir}"
-(
-  cd "${target_repo_dir}"
-  git add .
-  if git commit -am "${COMMIT_MESSAGE}" -m "${COMMIT_DETAILS}"; then
-      git show --name-status --format=format:
-      echo ''
-      echo "--- Push Changes"
-      git push 'origin' "${TARGET_BRANCH}"
-  fi
-)
+echo '--- Cleanup'
+echo "Remove Target Repository '${target_repo_dir}'"
+rm -rf "${target_repo_dir}"
